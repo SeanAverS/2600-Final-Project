98f957c645cd89b37a3e541184302d3345cdf685
Sean Avery <seanasuguitan@gmail.com>
2024-03-24 15:18:09 -0700

    cors install for api key access

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const config = require("./config/config");
 const utils = require("./utils");
 
 const app = express();
 
+app.use(cors());
 app.use(express.static(config.ROOT));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

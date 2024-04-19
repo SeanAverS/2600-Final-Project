9cbd75cd78db89b7c6902b8af1ad675e2f81c8c9
SeanAverS <seanasuguitan@gmail.com>
2024-04-18 22:53:58 -0700

    Make error messages visible

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -27,7 +27,7 @@ memberController.post('/signup', async (request, response) => {
         // check for existing user
         const existingUser = await collection.findOne({ email });
         if (existingUser) {
-            return response.status(400).json({ error: `${email} already exists. Choose a different email.` });
+            return response.status(400).json({ error: `<span class="text-danger">${email} already exists. Please choose a different email.</span>` });
         }
 
         await collection.insertOne({ email, password: hashedPassword });
@@ -55,7 +55,7 @@ memberController.post('/signin', async (request, response) => {
         const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
-            return response.status(400).json({ error: 'Invalid credentials' });
+            return response.status(400).json({ error: `<span class="text-danger">Please re-enter your email or password.</span>` });
         }
 
         response.status(200).json({ success: `${email} logged in successfully!` });
